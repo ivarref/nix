15ea6d69af26e3c75b4ca66eab4b29af0a88f0e7
ire <ivar.refsdal@sikt.no>
2024-08-12 11:35:18 +0200

    format

--- flake.nix
+++ flake.nix
@@ -13,157 +13,170 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    configuration = { pkgs, lib, ... }: {
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      home-manager,
+    }:
+    let
+      configuration =
+        { pkgs, lib, ... }:
+        {
 
-    # Auto upgrade nix package and the daemon service.
-    services.nix-daemon.enable = true;
-    # nix.package = pkgs.nix;
+          # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
+          # nix.package = pkgs.nix;
 
-    # Necessary for using flakes on this system.
-    nix.settings.experimental-features = "nix-command flakes";
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-    # Set Git commit hash for darwin-version.
-    system.configurationRevision = self.rev or self.dirtyRev or null;
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
 
-    # Used for backwards compatibility, please read the changelog before changing.
-    # $ darwin-rebuild changelog
-    system.stateVersion = 4;
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 4;
 
-    # The platform the configuration will be used on.
-    nixpkgs.hostPlatform = "aarch64-darwin";
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
 
-    fonts.packages = with pkgs; [
-      fira
-      fira-code
-      iosevka
-      roboto
-      roboto-mono
-      source-sans
-      source-code-pro
-      source-serif
-    ];
+          fonts.packages = with pkgs; [
+            fira
+            fira-code
+            iosevka
+            roboto
+            roboto-mono
+            source-sans
+            source-code-pro
+            source-serif
+          ];
 
-    # Declare the user that will be running `nix-darwin`.
-    users.users.ire = {
-      name = "ire";
-      home = "/Users/ire";
-    };
+          # Declare the user that will be running `nix-darwin`.
+          users.users.ire = {
+            name = "ire";
+            home = "/Users/ire";
+          };
 
-    programs.fish.enable = true;
-    system.activationScripts.postActivation.text = ''
-      # Set the default shell as fish for the user. MacOS doesn't do this like nixOS does
-      sudo chsh -s ${lib.getBin pkgs.fish}/bin/fish ire
+          programs.fish.enable = true;
+          system.activationScripts.postActivation.text = ''
+            # Set the default shell as fish for the user. MacOS doesn't do this like nixOS does
+            sudo chsh -s ${lib.getBin pkgs.fish}/bin/fish ire
 
-    '';
+          '';
 
-    security.pam.enableSudoTouchIdAuth = true;
+          security.pam.enableSudoTouchIdAuth = true;
 
-    # List packages installed in system profile. To search by name, run:
-    # $ nix-env -qaP | grep wget
-    environment.systemPackages =
-      [ pkgs.neofetch
-        pkgs.vim
-        pkgs.entr
-        pkgs.direnv
-        # pkgs.jdk22
-      ];
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.neofetch
+            pkgs.vim
+            pkgs.entr
+            pkgs.direnv
+            # pkgs.jdk22
+          ];
 
-    homebrew = {
-      enable = true;
-      # onActivation.cleanup = "uninstall";
-      # clone_target = "ssh";
-      # global = { config, ... }: {
-      # };
-      taps =  [ {
-                  name = "dmarcotte/easy-move-plus-resize";
-                  clone_target = "https://github.com/dmarcotte/easy-move-resize.git";
-                  force_auto_update = true;
-                }
-              ];
-      brews = [ "cowsay"
-                "fish"
-                "docker"
-                "lima"
-              ];
-      casks = [ "easy-move-plus-resize" ];
-      };
-    };
-    homeconfig = {pkgs, ...}: {
-        # this is internal compatibility configuration
-        # for home-manager, don't change this!
-        home.stateVersion = "23.05";
+          homebrew = {
+            enable = true;
+            # onActivation.cleanup = "uninstall";
+            # clone_target = "ssh";
+            # global = { config, ... }: {
+            # };
+            taps = [
+              {
+                name = "dmarcotte/easy-move-plus-resize";
+                clone_target = "https://github.com/dmarcotte/easy-move-resize.git";
+                force_auto_update = true;
+              }
+            ];
+            brews = [
+              "cowsay"
+              "fish"
+              "docker"
+              "lima"
+            ];
+            casks = [ "easy-move-plus-resize" ];
+          };
+        };
+      homeconfig =
+        { pkgs, ... }:
+        {
+          # this is internal compatibility configuration
+          # for home-manager, don't change this!
+          home.stateVersion = "23.05";
 
-        home.packages = with pkgs; [
-          bat
-          babashka
-          clj-kondo
-          clojure
-          fzf
-          neil
-          nixfmt-rfc-style
-          jdk
-          ripgrep
-          wget
-        ];
+          home.packages = with pkgs; [
+            bat
+            babashka
+            clj-kondo
+            clojure
+            fzf
+            neil
+            nixfmt-rfc-style
+            jdk
+            ripgrep
+            wget
+          ];
 
-        home.sessionVariables = {
+          home.sessionVariables = {
             EDITOR = "vim";
             CDPATH = "$HOME/code";
-        };
-
-        programs = {
-          fish = {
-            enable = true;
-            shellAliases = {
-                swdarwin = "darwin-rebuild switch --flake ~/.config/nix";
-            };
-            interactiveShellInit = ''
-            direnv hook fish | source
-            '';
           };
 
-          git = {
-            enable = true;
-            userName = "ire";
-            userEmail = "ivar.refsdal@sikt.no";
-            ignores = [ ".DS_Store" ];
-            extraConfig = {
+          programs = {
+            fish = {
+              enable = true;
+              shellAliases = {
+                swdarwin = "darwin-rebuild switch --flake ~/.config/nix";
+              };
+              interactiveShellInit = ''
+                direnv hook fish | source
+              '';
+            };
+
+            git = {
+              enable = true;
+              userName = "ire";
+              userEmail = "ivar.refsdal@sikt.no";
+              ignores = [ ".DS_Store" ];
+              extraConfig = {
                 init.defaultBranch = "main";
                 push.autoSetupRemote = true;
+              };
             };
+
+            direnv = {
+              enable = true;
+              nix-direnv.enable = true;
+            };
+
+            # Sets JAVA_HOME environment variable
+            java.enable = true;
+
+            # Let Home Manager install and manage itself.
+            home-manager.enable = true;
           };
-
-          direnv = {
-            enable = true;
-            nix-direnv.enable = true;
-          };
-
-          # Sets JAVA_HOME environment variable
-          java.enable = true;
-
-          # Let Home Manager install and manage itself.
-          home-manager.enable = true;
         };
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Ivars-MacBook-Air
-    darwinConfigurations."Ivars-MacBook-Air" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-        home-manager.darwinModules.home-manager  {
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Ivars-MacBook-Air
+      darwinConfigurations."Ivars-MacBook-Air" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.verbose = true;
             home-manager.users.ire = homeconfig;
-        }
-      ];
-    };
+          }
+        ];
+      };
 
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."Ivars-MacBook-Air".pkgs;
-  };
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."Ivars-MacBook-Air".pkgs;
+    };
 }
