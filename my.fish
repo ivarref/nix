951182a187f62d9293c2bf4dfd21b41e4f321fdb
ire <ivar.refsdal@sikt.no>
2024-08-20 14:20:31 +0200

    Add gfl => git ls-files | entr .. <last command>

--- my.fish
+++ my.fish
@@ -2,3 +2,9 @@
 abbr -a --set-cursor='%' -- db 'docker build --tag=dev:dev .%'
 abbr -a --set-cursor='%' -- gf 'git ls-files | entr -c bash -c \'%\''
 abbr -a --set-cursor='%' -- gc 'git commit -am "%" && git push'
+
+function last_history_item
+    echo "git ls-files | entr -c bash -c 'set -x; $history[1]'"
+end
+
+abbr -a gfl --function last_history_item
