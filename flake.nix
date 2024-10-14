9b4af138d38c6d54a82bc1c3e0822da8a22e8b4a
ire <ivar.refsdal@sikt.no>
2024-10-14 20:03:33 +0200

    add manpager ala bat

--- flake.nix
+++ flake.nix
@@ -155,6 +155,7 @@
                 set -gx DIRENV_LOG_FORMAT ""
                 direnv hook fish | source
                 export DOCKER_HOST=$(limactl list docker --format 'unix://{{.Dir}}/sock/docker.sock')
+                export MANPAGER="sh -c 'col -bx | bat -l man -p'"
                 source ~/.config/nix/my.fish
               '';
             };
