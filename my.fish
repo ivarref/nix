fdb662f7cc6583ad789177e7fb4f456a34205d46
ire <ivar.refsdal@sikt.no>
2024-10-14 20:13:38 +0200

    add git status abbr

--- my.fish
+++ my.fish
@@ -2,6 +2,7 @@
 abbr -a --set-cursor='%' -- db 'docker build --tag=dev:dev .%'
 abbr -a --set-cursor='%' -- gf 'git ls-files | entr -c bash -c \'%\''
 abbr -a --set-cursor='%' -- gc 'git commit -am "%" && git push'
+abbr -a --set-cursor='%' -- gs 'git status%'
 
 function last_history_item
     echo "git ls-files | entr -cr bash -c 'set -x; $history[1]'"
