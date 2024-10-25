fce1f8fc0988d22375218e047f72f1b9453be3b2
ire <ivar.refsdal@sikt.no>
2024-10-25 15:49:27 +0200

    Add neovim, add ntfs usage ++

--- flake.nix
+++ flake.nix
@@ -63,6 +63,24 @@
           system.activationScripts.postActivation.text = ''
             # Set the default shell as fish for the user. MacOS doesn't do this like nixOS does
             sudo chsh -s ${lib.getBin pkgs.fish}/bin/fish ire
+
+            # inspired by: https://www.reddit.com/r/MacOS/comments/16vmecr/ntfs_on_macos_sonoma_hacky_but_works/
+            sudo ln -sf $(readlink /usr/local/lib/libfuse-t.dylib) /usr/local/lib/libfuse.2.dylib
+            # ^^ symlink required by ntfs-3g
+
+            # example mount command:
+            # sudo bash -c 'mkdir /Volumes/NTFS && ntfs-3g /dev/disk4s1 /Volumes/NTFS'
+
+            # where disk4s1 is from
+            # $ diskutil list
+            # ...
+            # > /dev/disk4 (external, physical):
+            #:                       TYPE NAME                    SIZE       IDENTIFIER
+            # 0:     FDisk_partition_scheme                        *16.0 GB    disk4
+            # 1:               Windows_NTFS UUI                     16.0 GB    disk4s1
+            #                                                       HERE ------^^^^^^^
+
+            # sudo ln -sf $(readlink /usr/local/lib/libfuse-t.dylib) /usr/local/lib/libosxfuse.2.dylib
           '';
 
           security.pam.enableSudoTouchIdAuth = true;
@@ -70,10 +88,10 @@
           # List packages installed in system profile. To search by name, run:
           # $ nix-env -qaP | grep wget
           environment.systemPackages = [
-            pkgs.neofetch
             pkgs.vim
             pkgs.fish
             pkgs.iterm2
+            pkgs.ntfs3g
           ];
 
           homebrew = {
@@ -88,9 +106,21 @@
                 clone_target = "https://github.com/dmarcotte/easy-move-resize.git";
                 force_auto_update = true;
               }
+              {
+                name = "macos-fuse-t/homebrew-cask";
+                clone_target = "https://github.com/macos-fuse-t/homebrew-cask.git";
+                # fuse-t: https://github.com/macos-fuse-t/homebrew-cask/blob/main/Casks/fuse-t.rb
+                force_auto_update = true;
+              }
+              # {
+              #   name = "gromgit/homebrew-fuse";
+              #   clone_target = "https://github.com/gromgit/homebrew-fuse.git";
+              #   # mounty: https://github.com/gromgit/homebrew-fuse/blob/main/Casks/mounty.rb
+              #   force_auto_update = true;
+              # }
             ];
             brews = [ "cowsay" ];
-            casks = [ "easy-move-plus-resize" ];
+            casks = [ "easy-move-plus-resize" "fuse-t" ];
           };
         };
       homeconfig =
@@ -115,6 +145,7 @@
             fzf
             htop
             neil
+            neovim
             mc
             nixfmt-rfc-style
             jdk
--- my.fish
+++ my.fish
@@ -3,9 +3,12 @@ abbr -a --set-cursor='%' -- db 'docker build --tag=dev:dev .%'
 abbr -a --set-cursor='%' -- gf 'git ls-files | entr -c bash -c \'%\''
 abbr -a --set-cursor='%' -- gc 'git commit -am "%" && git push'
 abbr -a --set-cursor='%' -- gs 'git status%'
+abbr -a --set-cursor='%' -- fed 'nvim ~/.config/nix/flake.nix%'
+abbr -a --set-cursor='%' -- fedz 'zed ~/.config/nix/flake.nix%'
+abbr -a --set-cursor='%' -- fished 'nvim ~/.config/nix/my.fish%'
 
 function last_history_item
-    echo "git ls-files | entr -cr bash -c 'set -x; $history[1]'"
+    echo "git ls-files | entr -cr bash -c 'set -x; env FROM_ENTR=1 $history[1]'"
 end
 
 abbr -a gfl --function last_history_item
